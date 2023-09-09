ee0eb6cb1fcfa7ae46117fccc3592dc2ab5baf77
wxquare <wxquare@163.com>
2023-09-09 21:08:27 +0800

    Site updated: 2023-09-09 21:08:14

---
+++ reference/模型优化加速资料/DaSiamRPN/run_install.sh
@@ -0,0 +1,21 @@
+pip install --upgrade pip
+pip install numpy
+pip install opencv-python
+pip install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl
+
+
+DaSiam是个什么样的算法
+     · 是开源的吗？
+     · 代码多少行？
+     · 需要训练吗？用的什么数据去训练？用的是深度神经网络吗？
+     · 模型文件多大
+
+     · 运行框架有多大
+
+     · 在移动端有哪些框架 
+
+     · 模型文件能从pytorch转换为tensorflow格式吗?？？？？
+     · 转换模型后能否正常运行？对效果或者效率有什么影响？？？
+     · 模型文件如何才能精简？调整超参，调整数据集，在效果和模型大小、运行效率之间取平衡，应该怎么才能做到？
+     · 移动端的tensorflow mobile有多大？运行效率如何?能不能用到GPU？
+     · 移动端GPU编程，应该从哪里开始？
