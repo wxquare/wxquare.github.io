e045344b2f1ab62f2e15c2e062b891552f7231da
Xiangui Wang <Xiangui.Wang@shopee.com>
2026-05-21 12:09:56 +0800

    add code

--- books/ai-book/book/searcher-c2a407aa.js
+++ books/ai-book/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b3c481dd.js',
+                path_to_root + 'searchindex-1bdbf243.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
